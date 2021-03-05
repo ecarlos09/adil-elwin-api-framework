5433544f52bb9799a7874caedefcd82747c8c415
Elwin <elwin.carlos09@gmail.com>
2021-03-05 15:15:35 +0000

    add student route to app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const studentRoutes = require('../server/controllers/students');
+app.use('/students', studentRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to our Hamilton Cohort website!')
+});
+
+app.post('/', (req, res) => {
+    res.status(405).send('Not allowed to do that!');
 });
 
 module.exports = app;
